e88810bfaa3d99aab7e1ed7f89683f1bf3b6d975
AMILDER07 <amilder55@gmail.com>
2020-09-28 19:44:39 -0500

    me dio por ponerle constructor a cuentaPuntos

--- src/gestorAplicacion/usuario/CuentaPuntos.java
+++ src/gestorAplicacion/usuario/CuentaPuntos.java
@@ -8,6 +8,12 @@ public class CuentaPuntos {
     private int puntos;
 
     //Contructores de clase****
+    public CuentaPuntos(int cantidadCP, int numeroCuenta, Cliente titular, int puntos) {
+    	cantidadCuentasPuntos = cantidadCP;
+    	this.numeroCuenta = numeroCuenta;
+    	this.titular = titular;
+    	this.puntos = puntos;
+    }
 
     //Metodos GET and SET****
 
