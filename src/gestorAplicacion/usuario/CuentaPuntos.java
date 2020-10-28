d0a6d07dfb1f494c5dca9deeb5eee196a64291df
kmolano <kmolano@gmail.com>
2020-10-28 01:52:47 -0500

    Vertigo

--- src/gestorAplicacion/usuario/CuentaPuntos.java
+++ src/gestorAplicacion/usuario/CuentaPuntos.java
@@ -4,17 +4,20 @@ import baseDatos.BaseDeDatos;
 
 import java.io.Serializable;
 
+/*Clase que guardara la informacion del usuario respecto a los puntos*/
 public class CuentaPuntos implements Serializable {
+
     //Atributos de clase****
     private int numeroCuenta;
     private transient Cliente titular;
     private int puntos;
 
     //Contructores de clase****
-    public CuentaPuntos(){
+    public CuentaPuntos() {
         this.numeroCuenta = BaseDeDatos.getCuentasPuntos().size();
         this.puntos = 10000;
     }
+
     public CuentaPuntos(Cliente titular) {
         this();
         this.titular = titular;
